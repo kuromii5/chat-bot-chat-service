660f98b62f67936da57f218c1515180b9f8b4c93
kuromii5 <aleksandrstepanov771@gmail.com>
2026-02-04 00:40:56 +0300

    fix validator

--- pkg/validator/validator.go
+++ pkg/validator/validator.go
@@ -2,6 +2,8 @@ package validator
 
 import (
 	"fmt"
+	"reflect"
+	"strings"
 
 	"github.com/go-playground/validator/v10"
 )
@@ -10,10 +12,18 @@ var validate *validator.Validate
 
 func Init() {
 	validate = validator.New()
+
+	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
 }
 
 type ValidationError struct {
-	Fields map[string]string
+	Fields map[string]string `json:"fields"`
 }
 
 func (e ValidationError) Error() string {
@@ -25,11 +35,32 @@ func Validate(s any) error {
 		if validationErrors, ok := err.(validator.ValidationErrors); ok {
 			fields := make(map[string]string)
 			for _, fieldErr := range validationErrors {
-				fields[fieldErr.Field()] = fmt.Sprintf("failed validation: %s", fieldErr.Tag())
+				fields[fieldErr.Field()] = formatMessage(fieldErr)
 			}
 			return ValidationError{Fields: fields}
 		}
 		return err
 	}
 	return nil
+}
+
+func formatMessage(err validator.FieldError) string {
+	switch err.Tag() {
+	case "required":
+		return "This field is required"
+	case "email":
+		return "Invalid email format"
+	case "oneof":
+		return fmt.Sprintf("Value must be one of the following: %s", err.Param())
+	case "min":
+		return fmt.Sprintf("Minimum length or value: %s", err.Param())
+	case "max":
+		return fmt.Sprintf("Maximum length or value: %s", err.Param())
+	case "uuid":
+		return "Must be a valid UUID"
+	case "tags":
+		return "Must be a valid tags"
+	default:
+		return fmt.Sprintf("Invalid value (error code: %s)", err.Tag())
+	}
 }
